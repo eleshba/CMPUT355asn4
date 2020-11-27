173308c027b93c49f545c932c061694e4b19de9d
Jorge Marquez <marquezp@gmail.com>
2020-11-26 21:40:32 -0700

    commented code

--- snake/Head.py
+++ snake/Head.py
@@ -1,6 +1,4 @@
 import pygame
-import time
-
 
 class Head:
     black = (0, 0, 0)
@@ -10,12 +8,14 @@ class Head:
         self.coordinates = coordinates
         self.velocity = velocity
 
+    # change the coordinates in the direction of the velocity
     def move_head(self):
         new_x = self.coordinates[0] + self.dimension * self.velocity[0]
         new_y = self.coordinates[1] + self.dimension * self.velocity[1]
 
         self.coordinates = (new_x, new_y)
 
+    # draw the snakes head
     def draw_head(self, screen):
         rect = pygame.Rect(self.coordinates, (10, 10))
         pygame.draw.rect(screen, self.black, rect)
--- snake/Snake.py
+++ snake/Snake.py
@@ -12,35 +12,40 @@ class Snake:
         self.body = deque([])
         self.head = Head.Head(self.starting_pos, (0, 0))
 
+    # draw the snake
     def draw_snake(self, screen):
         self.head.draw_head(screen)
         for body_part in self.body:
             rect = pygame.Rect(body_part, self.body_size)
             pygame.draw.rect(screen, self.purple, rect)
 
+    # changes the velocity vector of the head
     def change_dir(self, new_velocity):
         if (self.head.velocity[0] == 0) and (self.head.velocity[1] == 0):   # starting velocity is zero
             self.head.velocity = new_velocity
-        else:
+        else:  # make sure we cant move in the complete opposite direction
             if self.head.velocity[0] != -(new_velocity[0]):
                 if self.head.velocity[1] != -(new_velocity[1]):
                     self.head.velocity = new_velocity
 
+    # Update positions by having each previous node take on the position of the following node
     def move_snake(self):
         # move the body
         if len(self.body) > 0:
-            self.body.rotate(1)
+            self.body.rotate(1) # move the coordinates over
             self.body.popleft()
             self.body.appendleft(self.head.coordinates)
         # move the head
         self.head.move_head()
 
+    # Grow the snake by appending a new set of coordinates to the body
     def grow(self):
         if not self.body:
             self.body.append(self.head.coordinates)
         else:
             self.body.append(self.body[-1])
 
+    # Detects if the head is in contact with any of the body parts
     def istouching(self):
         for body_part in self.body:
             if self.head.coordinates == body_part:
